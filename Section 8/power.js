f3fa4a9bfe9cb2c6fbd18ee11b9ba3151f865b6b
osama-mustafa <semsem6661@gmail.com>
2024-08-10 14:04:45 +0200

    Fix First Problem

--- Section 8/power.js
+++ Section 8/power.js
@@ -2,15 +2,11 @@
 // power(2,2) // 4
 // power(2,4) // 16
 
-
 function power(base, expo){
     if (expo == 0) return 1;
-    return power(base, expo) * power(base, expo - 1);
+    return base * power(base, expo - 1);
 }
 
-power(2,0) // 1
-power(2,2) // 4
-power(2,4) // 16
-
-
-
+console.log(power(2, 0));
+console.log(power(2, 2));
+console.log(power(2, 4));
